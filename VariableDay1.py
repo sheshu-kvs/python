745e7d527aae22a0b02d2f8d44d8cbde325a2661
Sheshadri <kvs24161@gmail.com>
2026-04-06 22:46:06 +0530

    first python programs

---
+++ VariableDay1.py
@@ -0,0 +1,64 @@
+# A variable are used to store the data
+# x=5;
+# y=10;
+# print(x,"",y);
+
+
+# Rules of the Python Declaring the Variable
+# A Variable start with the lettar or the underscore
+# A variable cannot start with the numbers
+# A Variable can contain with the alphanumeric values a-z or the 0-9, _
+# A Variable can start with the _underscore
+
+
+# a=5;  # int 
+# b="5"; #string
+# print(a==b)
+
+# Typecasting
+#   an type casting are used to the convert the one data type to the other datatype
+
+# i=10;
+# z=str(i);
+
+# print(type(z));
+
+
+# Multiple variable are used to the 
+# An Multipe Variable are used to the Declare the multiple variables on the single line 
+
+# a,b,c = 1,2,3;
+# print(c);
+
+
+# x=y=z=90
+# print(x)
+# print(y)
+# print(z)
+
+
+# output Based Qt 
+
+# x = 10
+# y = x
+# x = 20
+# print(y)
+
+
+# a = [1, 2, 3]
+# b = a
+# a.append(4)
+# print(b)
+
+a = [1, 2, 3]
+b = a
+print(b)
+
+a = [1, 2, 3]
+b = a.copy()
+
+print(b);
+
+
+# Global Varible & local Variable 
+
