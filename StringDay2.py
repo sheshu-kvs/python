99dbc9fc81c1716c03a45b7557afc867390f3f06
Sheshadri <kvs24161@gmail.com>
2026-04-07 06:49:51 +0530

    Sring Exercise

---
+++ StringDay2.py
@@ -0,0 +1,80 @@
+# String in th python
+# a string in the pyton are used to the it is enclose with the single quotes & double quotes
+
+# quotes inside a string
+# single quote 
+# str= "deva is my 'friend'";
+# print(str)
+
+# double quote inside the string
+# str='deva is my "friend"';
+# print(str)
+
+# multiline string
+# str="""
+# deva is my friend of,
+# the feature of the mine,
+# there is no magic is there
+# inside in the java
+# """
+
+# print(str)
+
+
+# finding the lenght of the string using len()
+
+# str="kumar"
+# print(len(str))
+
+
+
+# finding the specific word in the sring
+
+# str= "deva is one of my frnd"
+
+# if "frnd" in str:
+#     print("word present in str");
+
+# str= "deva is one of my frnd"
+
+# for x in str:
+#     print(x);
+
+
+# reverse the string
+# str="sheshadri"
+# rev = "";
+# for x in str:
+#     rev = " " + x + rev;
+# print(rev);
+
+
+# palindrome of the string
+
+# str = "mam";
+# rev=''
+# for x in str:
+#     rev = x + rev;
+# if(rev==str):
+#     print(str," yes it is the palin string");
+# else:
+#     print(str," yes it is not the palin string");
+
+# reverse string using the using the stop, step in the for loop
+str="mam";
+print(len(str));
+rev ="";
+# for x in range(len(str)-1, -1, -1):
+
+for x in range()
+
+
+
+  
+
+
+
+
+
+
+
