143f3fc9a6df58d14bfadad8a4b417200a980a6f
Sheshadri <kvs24161@gmail.com>
2026-04-13 23:16:03 +0530

    Converting the UpperCase without using the upper()

---
+++ progDay1StringProg.py
@@ -0,0 +1,15 @@
+a="acde";
+
+# val=""
+asciival =[ord(char) for char in a]
+# if(asciival >= 97 or asciival <= 122):
+for val in asciival:
+    if(val>=97 or val <=122):
+        print(chr(val-32))
+
+
+    
+
+
+
+
