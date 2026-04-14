cb1dd3ce4637c0f2749a28941a27412a6b13b42f
Sheshadri <kvs24161@gmail.com>
2026-04-14 22:41:34 +0530

    List pyp

---
+++ list.py
@@ -0,0 +1,34 @@
+# a list is the collection which is ordered , changeable, duplicates allowed
+
+
+a=['a','b','c'];
+# print(a)
+
+# an list can be the same dt
+
+a=['a','b','c'];
+# print(a)
+
+# an list can be the different datatype
+a=[12,'sdr',34.5]
+print(a);
+
+# changing the list at the specific item
+a=[12,'sdr',34.5];
+a[1]="merry";
+print(a)
+
+
+# chnaging the range of the list
+a=[12,'sdr',34.5];
+a[0:2] = ["daya","daya","daya"]
+print(a);
+
+
+b=['daya', 'daya', 'daya', 34.5]
+b[0:3]=["changedname"];
+val=""
+# for 'daya' in b:
+
+    
+     
