4eb2d3732f6c827c43c4da1d7107f2d6eb4281a9
Sheshadri <kvs24161@gmail.com>
2026-04-22 06:37:45 +0530

    python prgm

---
+++ listprogpractise.py
@@ -0,0 +1,18 @@
+# Check if an element exists in a list
+
+# a=[1,2,3,34,334,53];
+# search=1;
+# for x in a:
+#     if(search == x):
+#         print('Item Found',x);
+#         break;
+# else:
+#     print('Item Not Found ');
+
+
+# Reverse a list
+a=[1,2,3,34,334,53];
+# start len(a)-1
+# stop 1
+for x in range(len(a)-1, 1, -1):
+    print(x);
