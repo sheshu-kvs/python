6da3345dc2092a035f98e793ab986277683db0df
Sheshadri <kvs24161@gmail.com>
2026-07-04 16:47:25 +0530

    Python Str,type,airthmatic operators

---
+++ 30dayspython/Day1/helloworld.py
@@ -0,0 +1,75 @@
+# Python Version 3.13.2
+
+
+
+# Q1
+# addition(+) the pytho
+# subtraction(-)
+# multiplication(*)
+# modulus(%)
+# division(/)
+# exponential(**)
+# floor division operator(//)
+
+
+
+a=3;
+b=4;
+
+print("Add ",(a+b));
+print("Sub ",(a-b));
+print("Mul ",(a*b));
+print("Div ",(a/b));
+print("Modulas ",(a%b));
+print("Exponential ",(a**b));
+print("Floor Divisipon ",(a//b));
+
+
+
+# Q2
+# Write strings on the python interactive shell. The strings are the following:
+# Your name
+# Your family name
+# Your country
+# I am enjoying 30 days of python
+print("**************************Quetion  2**************************")
+name="Deva";
+fam_name="kadavakal";
+country="bharat";
+val="I am Enjoying 30 days of python";
+
+print("Name ",name);
+print("FamilyName ",fam_name);
+print("Country",country);
+print("String ",val);
+
+
+# Q3
+# Check the data types of the following data:
+# 10
+# 9.8
+# 3.14
+# 4 - 4j
+# ['Asabeneh', 'Python', 'Finland']
+# Your name
+# Your family name
+# Your country
+
+num1=10;
+num2=9.8;
+num3=3.14;
+num4=4 - 4j
+lst = ['Asabeneh', 'Python', 'Finland'];
+tup= ("a","b")
+setval={1,3,"deva"};
+dictval={"car":"bmw","model":"b4"}
+
+
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(num4))
+print(type(lst))
+print(type(setval))
+print(type(dictval))
