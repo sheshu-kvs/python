5ed047150fd2483bf6e3fbf65c61614e8d04bac9
Sheshadri <kvs24161@gmail.com>
2026-07-10 06:55:11 +0530

    functions

---
+++ 30dayspython/Day11/functions.py
@@ -0,0 +1,14 @@
+# Declare a function add_two_numbers. It takes two parameters and it returns a sum.
+def sum(a,b):
+    add=a+b;
+    print(add);
+
+sum(10,5);
+
+
+def arc():
+    radius=2;
+    areaofcircle=3.14*2*2;
+    print(areaofcircle);
+
+arc();
