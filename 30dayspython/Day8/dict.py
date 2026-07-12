80841f883f7dc734a376b0c26a66210aa9745840
Sheshadri <kvs24161@gmail.com>
2026-07-12 20:03:59 +0530

    Update Loops In the Dictionary Practised Some Questions

--- 30dayspython/Day8/dict.py
+++ 30dayspython/Day8/dict.py
@@ -17,4 +17,65 @@ print(dog.values())
 newdoglst=list(dog);
 print(newdoglst)
 
-print(dog.pop(1));
+# print(dog.pop(1));
+
+
+
+
+# dictionary Practising session
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+
+# print(len(dict));
+
+person={
+    'first_name':'sheshu',
+    'last_name':'kumar',
+    'is_married':True,
+     'skills':['html','css','js','python','sql'],
+     'address':{
+         'hometown':'madhavaram',
+         'living':'banglore'
+     }
+}
+
+# len of the str
+print(len(person));
+
+print(person['first_name']);
+print(person['last_name']);
+print(person['skills']);
+print(person['address']);
+
+person['skills'].append("Java")
+print(person['skills'])
+
+# if the particular item is present or not in the in the dictionary
+# print('skills' in person)  
+
+# print('address' in person)
+# print('first_name' in person)
+
+#popitem are used to delete the last item in the dictionary 
+# pop are used to delete the 
+
+
+print(person.items());
+# is used to convert the dictionary  to the list
+# print(person)
+
+
+# 
+
+
+ 
