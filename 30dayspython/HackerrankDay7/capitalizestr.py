2c38602117f637e33fd9027f6799801a2aed68cb
Sheshadri <kvs24161@gmail.com>
2026-07-29 22:34:16 +0530

    first char to be upper

--- 30dayspython/HackerrankDay7/capitalizestr.py
+++ 30dayspython/HackerrankDay7/capitalizestr.py
@@ -1,8 +1,56 @@
-list31=['dal','dem','mena'];
-lisd2=str(list31);
-lisd2.capitalize()
-print(lisd2)
+# # list31=['dal','dem','mena'];
+# # lisd2=str(list31);
+# # lisd2.capitalize()
+# # print(lisd2)
+
+# # dw='sre gjnjn'
+# # print(dw.title())
+# strval='deva mani';
+# splval = strval.split(" ");
+# t=""
+# for i in range(len(splval)):
+#     # print(i)
+#     # print(len(splval[i]));
+#     for j in range(len(splval[i])):
+#         # print(splval[i][j])
+#         if splval[i][j].isdigit():
+#             t=t+splval[i][j];
 
 
-dw='sre gjnjn'
-print(dw.title())
+
+# val='deva';
+# t='';
+# if val[0].isdigit():
+#     print(val);
+# else:
+#     upval=val[0].upper();
+#     print(upval)
+
+
+# only single value
+# val='deva'
+# t="";
+# for i in range(len(val)):
+#     if i==0:
+#         t=val[i].upper();
+#     else:
+#         t=t+val[i];
+
+# print(t)
+
+
+
+val='deva mariya';
+t='';
+splval=val.split(' ');
+for word in range(len(splval)):
+    for char in range(len(splval[word])+1):
+        if char==0:
+            t=t+splval[word][char].upper();
+        elif char<len(splval[word]):
+              t=t+splval[word][char];
+        elif char==len(splval[word]):
+              if word<len(splval)-1:
+                   t=t+' '; 
+
+print(t)
