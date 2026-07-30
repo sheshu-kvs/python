5883aa18a8c9abec96549db482601ae51b738566
Sheshadri <kvs24161@gmail.com>
2026-07-30 22:31:29 +0530

    first lettar to be the capital in the easch word

--- 30dayspython/HackerrankDay7/capitalizestr.py
+++ 30dayspython/HackerrankDay7/capitalizestr.py
@@ -40,17 +40,44 @@
 
 
 
-val='deva mariya';
-t='';
-splval=val.split(' ');
-for word in range(len(splval)):
-    for char in range(len(splval[word])+1):
-        if char==0:
-            t=t+splval[word][char].upper();
-        elif char<len(splval[word]):
-              t=t+splval[word][char];
-        elif char==len(splval[word]):
-              if word<len(splval)-1:
-                   t=t+' '; 
+# val='deva mariya';
+# t='';
+# splval=val.split(' ');
+# for word in range(len(splval)):
+#     for char in range(len(splval[word])+1):
+#         if char==0:
+#             t=t+splval[word][char].upper();
+#         elif char<len(splval[word]):
+#               t=t+splval[word][char];
+#         elif char==len(splval[word]):
+#               if word<len(splval)-1:
+#                    t=t+' '; 
 
-print(t)
+# print(t)
+
+
+
+# str12=input("Enter the String");
+
+# if any(char.isdigit() for char in str12):
+#     print(str12);
+# else:
+#     res=str12.title();
+#     print(res);
+
+
+str12=input("Enter the String:");
+val=str12.split( );
+newval=[]
+for word in val:
+    words=''
+    for ch_idx in range(len(word)):
+        if ch_idx==0:
+            words=word[ch_idx].upper();
+        else:
+            words=words+word[ch_idx];
+    newval.append(words)
+res=" ".join(newval)
+print(res);
+
+
