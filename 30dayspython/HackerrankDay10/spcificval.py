e2938f3b2723804c683bf51ea2b7d8492ae7df04
Sheshadri <kvs24161@gmail.com>
2026-08-03 06:52:45 +0530

    4 char each & prod result

--- 30dayspython/HackerrankDay10/spcificval.py
+++ 30dayspython/HackerrankDay10/spcificval.py
@@ -1,10 +1,23 @@
 str12='ABCDEFGHIJKLIMNOQRSTUVWXYZ';
 max=4;
+print(len(str12))
 # slicing
 # for i in range(0,len(str12),4):
 #     print(str12[i:i+4]);
 
 for i in range(0,len(str12),4):
     for j in range(i,i+4):
-        print(str12[j],end=" ");
+        if j<len(str12):
+            print(str12[j],end=" ");
     print(" ") 
+
+
+
+val1=[1,2]
+val2=[3,4]
+for i in range(len(val1)):
+    for j in range(len(val2)):
+        print(f"({val1[i]},{val2[j]})")    
+
+a=[1,2];
+b=[3,4];
