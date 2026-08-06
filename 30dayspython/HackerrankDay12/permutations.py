df7e78efb0ea61bf1191f73b32adfe5279f16090
Sheshadri <kvs24161@gmail.com>
2026-08-06 22:45:56 +0530

    comintions in te all the format

--- 30dayspython/HackerrankDay12/permutations.py
+++ 30dayspython/HackerrankDay12/permutations.py
@@ -1,7 +1,10 @@
 from itertools import permutations
 val=input().split();
-val1=val[0];
+val1=sorted(val[0]);
 val2=int(val[1]);
 am=permutations(val1,val2);
-for res in am:
-    print(" ".join(res));
+for i in range(1,val2+1):
+    for res in permutations(val1,i):
+        print(" ".join(res).upper());
+
+
