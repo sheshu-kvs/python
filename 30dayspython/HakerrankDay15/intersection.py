d122c72de3c42fd9f7b5026a9c34cc95720cba70
Sheshadri <kvs24161@gmail.com>
2026-08-08 07:10:32 +0530

    intersection

---
+++ 30dayspython/HakerrankDay15/intersection.py
@@ -0,0 +1,11 @@
+input();
+val1=set(map(int,input().split()));
+input();
+val2=set(map(int,input().split()));
+l1=len(val1);
+l2=len(val2)
+print(val1)
+print(val2)
+
+print(len(val1.intersection(val2)))
+# print(l1+l2)
