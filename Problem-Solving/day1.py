dd201bae53e14322549e3ea4711783fc9969dcba
Sheshadri <kvs24161@gmail.com>
2026-09-04 05:58:42 +0530

    >3 & Vowels or Conso

--- Problem-Solving/day1.py
+++ Problem-Solving/day1.py
@@ -16,6 +16,29 @@ def swapvalues():
     a=a-b;
     print("The a Val",a)
     print("The b Val",b)
-    
+
+
+def greatestofthree():
+    a=int(input())
+    b=int(input())
+    c=int(input())
+    if a>b and a>c:
+        print(a ,"is greater");
+    elif b>c and b>a:
+        print(b,"is greater");
+    elif c>a and c>b:
+        print(c,"is greater");
+
+
+def vowelorconso():
+    val=input();
+    lw=val.lower();
+    if lw=='a' or lw=='e'or lw=='i'or lw=='o'or lw=='u':
+        print(lw,"is Vowels");
+    else:
+        print(lw,"is Consonent");
+
 # findrectangle()
 # swapvalues()
+# greatestofthree()
+vowelorconso();
