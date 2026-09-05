58e9cd81657862be3994c15d171ad91133dedd4f
Sheshadri <kvs24161@gmail.com>
2026-09-05 22:17:55 +0530

    using the pop,discard,remove with the set

--- 30dayspython/HakerrankDay15/intersection.py
+++ 30dayspython/HakerrankDay15/intersection.py
@@ -1,63 +1,27 @@
-input();
-val1=set(map(int,input().split()));
-input();
-val2=set(map(int,input().split()));
-l1=len(val1);
-l2=len(val2)
-print(val1)
-print(val2)
-
-print(len(val1.intersection(val2)))
-# print(l1+l2)
 
 
-
-def spacenum():
-    n=int(input());
-    sp=6;
-    for i in range(1,n+1):
-        for j in range(1,i+1):
-            print(j,end="");
-        for j in range(sp):
-            print(" ",end="")
-        for k in range(i,0,-1):
-            print(k,end="");
-        sp-=2;
-        print()
-# spacenum();
-
-
-def printnum():
-
-    n=int(input())
-    num=1;  
-    for i in range(1,n+1):
-        # num=1;
-        for j in range(1,i+1):
-            print(num,end="");
-            num+=1;
-        print()
-# printnum();
-
-def printalpha():
-  
-    for i in range(1,6):
-        ch=65;
-        for j in range(ch,ch+i):
-            print(chr(j),end="");
-            ch=ch+1;
-        print()
-
-# printalpha();
+def setremove():
+    # n=int(input())
+    s12=set(map(int,input().split()))
+    num=int(input())
+    
+    
+    for i in range(num):
+        ip=input().split()
+        if len(ip)>1:
+            if ip[0]=="remove":
+                 val2=int(ip[1]);
+                 s12.remove(val2);
+            elif ip[0]=="discard":
+                 val2=int(ip[1]);
+                 s12.discard(val2);
+        elif len(ip)==1 and ip[0]=="pop":
+            s12.pop();
+    
+    print(int(s12))
 
 
-def reversealph():
-    for i in range(5,6):
-        ch=65;
-        
-        for j in range(ch+):
-            print(chr(j),end="")
-        ch+=1;
-        print()
 
-reversealph()
+
+
+setremove();
